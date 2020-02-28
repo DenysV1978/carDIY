42dc25b4152fdc315ad350ddabfe0c3b00615767
denysV1978 <denysvuk@gmail.com>
2020-02-28 15:13:52 -0600

    Creation of GIT repo

---
+++ src/main/java/org/launchcode/carDIY/CarDiyApplication.java
@@ -0,0 +1,15 @@
+package org.launchcode.carDIY;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarDiyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarDiyApplication.class, args);
+
+		System.out.println("Hello Denys");
+	}
+
+}
---
+++ src/test/java/org/launchcode/carDIY/CarDiyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.carDIY;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarDiyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
